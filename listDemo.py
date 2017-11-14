57c3bb2346efd61e2833dc9490fdbb3e5cf0c6f2
garyyli <garyli@hanovernorwichschools.org>
2017-11-14 14:21:02 -0500

    Updated from Brython Server: 11/14/2017 2:21:04 PM

--- listDemo.py
+++ listDemo.py
@@ -7,3 +7,6 @@ words = input('Enter a list of words: ').split(' ')
 #print out the list one item per line
 for w in words:
     print(w)
+
+print('The first word is', words[0])
+print('The last word is', words[-1])
