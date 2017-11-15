fb90d3d0694484ac00384a3e6a27cc87bbcf6106
garyyli <garyli@hanovernorwichschools.org>
2017-11-15 13:57:45 -0500

    Updated from Brython Server: 11/15/2017 1:57:47 PM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,13 @@
+#Gary Li
+#11/15/17
+#middleWord.py
 
+words = input('Enter a list of words: ').split(' ')
+words.sort()
+
+middle = len(words)
+
+if middle%2 == 0:
+    print(words[(middle/2)-1], words[middle/2])
+else:
+    print(words[middle//2])
