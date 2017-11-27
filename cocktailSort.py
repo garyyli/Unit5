1b1bd8559e5074ba29a3e29cb08ffc6a7c2812f7
garyyli <garyli@hanovernorwichschools.org>
2017-11-27 14:45:43 -0500

    Updated from Brython Server: 11/27/2017 2:45:43 PM

--- cocktailSort.py
+++ cocktailSort.py
@@ -5,30 +5,7 @@
 from random import randint
 from time import time
 
-N = 100 #how many numbers will be sorted
-
-'''procedure cocktailShakerSort( A : list of sortable items ) defined as:
-  do
-    swapped := false
-    for each i in 0 to length( A ) - 2 do:
-      if A[ i ] > A[ i + 1 ] then // test whether the two elements are in the wrong order
-        swap( A[ i ], A[ i + 1 ] ) // let the two elements change places
-        swapped := true
-      end if
-    end for
-    if not swapped then
-      // we can exit the outer loop here if no swaps occurred.
-      break do-while loop
-    end if
-    swapped := false
-    for each i in length( A ) - 2 to 0 do:
-      if A[ i ] > A[ i + 1 ] then
-        swap( A[ i ], A[ i + 1 ] )
-        swapped := true
-      end if
-    end for
-  while swapped // if no elements have been swapped, then the list is sorted
-end procedure'''
+N = 500 #how many numbers will be sorted
 
 def mySort(A):
     swapped = True
