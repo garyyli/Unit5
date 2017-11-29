51a72bd59aabc727db7c3f755da98b2901b49ae6
garyyli <garyli@hanovernorwichschools.org>
2017-11-29 13:42:53 -0500

    Updated from Brython Server: 11/29/2017 1:42:52 PM

--- insertionSort.py
+++ insertionSort.py
@@ -7,16 +7,6 @@ from time import time
 
 N = 100 #how many numbers will be sorted
 
-'''i ← 1
-while i < length(A)
-    j ← i
-    while j > 0 and A[j-1] > A[j]
-        swap A[j] and A[j-1]
-        j ← j - 1
-    end while
-    i ← i + 1
-end while'''
-
 def mySort(A):
     i=1
     while i<len(A):
