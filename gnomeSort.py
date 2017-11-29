6151cc730dcbdd500812faf8b861a9580f4df141
garyyli <garyli@hanovernorwichschools.org>
2017-11-29 13:44:56 -0500

    Updated from Brython Server: 11/29/2017 1:44:55 PM

--- gnomeSort.py
+++ gnomeSort.py
@@ -1,1 +1,42 @@
+#Gary Li
+#11/29/17
+#gnomeSort.py - implematation of gnome sort
 
+from random import randint
+from time import time
+
+N = 100 #how many numbers will be sorted
+
+'''procedure gnomeSort(a[]):
+    pos := 0
+    while pos < length(a):
+        if (pos == 0 or a[pos] >= a[pos-1]):
+            pos := pos + 1
+        else:
+            swap a[pos] and a[pos-1]
+            pos := pos - 1'''
+
+def mySort(A):
+
+    return A
+
+if __name__ == '__main__':
+    
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+        
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = mySort(numbers)
+    t2 = time()
+       
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
