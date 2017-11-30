e58253613b3bd0fe7118ada3a65a3a47a098ffd6
garyyli <garyli@hanovernorwichschools.org>
2017-11-30 14:24:01 -0500

    Updated from Brython Server: 11/30/2017 2:24:02 PM

--- bubbleSort.py
+++ bubbleSort.py
@@ -7,22 +7,6 @@ from time import time
 
 N = 100 #how many numbers will be sorted
 
-'''procedure bubbleSort( A : list of sortable items )
-    n = length(A)
-    repeat 
-        swapped = false
-        for i = 1 to n-1 inclusive do
-            /* if this pair is out of order */
-            if A[i-1] > A[i] then
-                /* swap them and remember something changed */
-                swap( A[i-1], A[i] )
-                swapped = true
-            end if
-        end for
-    until not swapped
-end procedure
-'''
-
 def mySort(A):
     n = len(A)
     swapped = True
