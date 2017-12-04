4b626e3bacd8ebded2ca4a20248475d9c70d4084
garyyli <garyli@hanovernorwichschools.org>
2017-12-04 14:33:38 -0500

    Updated from Brython Server: 12/4/2017 2:33:38 PM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,16 @@
+#Gary Li
+#12/4/17
+#quiz5.py
+
+from random import randint
+
+randomInt = randint(1,100)
+def rand5():
+    numbers = []
+    for i in range(0,5):
+        randomInt = randint(1,100)
+        numbers.append(randomInt)
+    return numbers
+
+print(rand5())
 
